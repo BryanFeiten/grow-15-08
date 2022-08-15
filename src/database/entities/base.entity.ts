f0d6a962e244a1482d1ccb8172f1aa5e715829f2
Bryan <bryan.feiten@growdev.com.br>
2022-08-15 20:33:44 -0300

    feat; cria entidade base para geração de informações básicas que todas tabelas terão

---
+++ src/database/entities/base.entity.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from "crypto";
+import {BeforeInsert, Entity, PrimaryColumn} from "typeorm";
+
+@Entity()
+export class EntityBase {
+  @PrimaryColumn()
+  public uid!: string;
+
+  @BeforeInsert()
+  public beforeInsert(): void {
+    this.uid = randomUUID();
+  }
+}
