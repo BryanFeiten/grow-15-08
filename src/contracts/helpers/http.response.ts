e1aae68022f51eb90950980f75d9592d9756118b
Bryan <bryan.feiten@growdev.com.br>
2022-08-24 19:55:54 -0300

    feat: cria padronização nas possíveis HttpResponses

--- src/contracts/helpers/http.response.ts
+++ src/contracts/helpers/http.response.ts
@@ -1,11 +1,45 @@
 export interface HttpResponse {
+  success: boolean,
   body: any,
   code: number,
 }
 
-export const ResponseSuccess = (content: any): HttpResponse => {
-  return {
-    body: content,
-    code: 200,
-  }
+export const Response = {
+  success: (content: any): HttpResponse => {
+    return {
+      success: true,
+      body: content,
+      code: 200,
+    }
+  },
+  failure: (code: number, process: string, error: any): HttpResponse => {
+    return {
+      success: false,
+      body: {
+        process,
+        error,
+      },
+      code
+    }
+  },
+  notFound: (process: string, error: any): HttpResponse => {
+    return {
+      success: false,
+      body: {
+        process,
+        error,
+      },
+      code: 404
+    }
+  },
+  serverFailure: (process: string, error: any): HttpResponse => {
+    return {
+      success: false,
+      body: {
+        process,
+        error,
+      },
+      code: 404
+    }
+  },
 }
