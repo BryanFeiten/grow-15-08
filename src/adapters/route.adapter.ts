c76941bce3545f2c9f1b6b84727023eb44bbe1e5
Bryan <bryan.feiten@growdev.com.br>
2022-08-24 20:14:42 -0300

    feat: implementa tratamento de erros correto

--- src/adapters/route.adapter.ts
+++ src/adapters/route.adapter.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import { CustomError } from "../errors";
-import { HttpResponse } from '../contracts/helpers/http.response';
+import { HttpResponse, Response as AppResponse } from '../contracts/helpers/http.response';
 import { HttpRequest } from "../contracts/helpers/http.request";
 
 export const adaptRoute = (controller: any) => {
@@ -10,29 +10,17 @@ export const adaptRoute = (controller: any) => {
       params: req.params,
       query: req.query,
     };
-    let httpResponse: HttpResponse = {
-      code: 500,
-      body: {},
-    };
 
     try {
-      httpResponse = await controller(httpRequest);
-    } catch (error) {
-      if (error instanceof CustomError) {
-        httpResponse.code = error.code;
-        httpResponse.body = {
-          process: error.process,
-          message: error.message,
-        };
-      } else {
-        httpResponse.code = 500;
-        httpResponse.body = {
-          process: 'Desconhecido',
-          message: 'Erro interno',
-        };
-      }
-    }
+      const httpResponse = await controller(httpRequest);
 
-    return res.status(httpResponse.code).json(httpResponse.body);
+      return httpResponse;
+    } catch (error: any) {
+      if (error instanceof CustomError) {
+        return AppResponse.failure(error.code, error.process, error.message, error)
+      }
+      
+      return AppResponse.serverFailure(error.process, error);
+    }
   }
 }
